2af5fdee977dddb0ecc6507fffba91b19b316fd8
201519960121lsy <760143003@qq.com>
2019-12-24 11:31:57 +0800

    页面不è刷新登陆

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -14,9 +14,19 @@ function post() {
             if (response.code==200){
                 $("#comment_section").hide();
             }else{
-                alert(response.message);
+                //未登录
+                if (response.code==2003){
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted){
+                        window.open("https://github.com/login/oauth/authorize?client_id=Iv1.826e125ed735f67a&redirect_uri=http://localhost:8887/callback&scope=user&state=1");
+                       //本地存储
+                        window.localStorage.setItem("closable",true);
+                    }else{
+                        alert(response.message);
+                    }
+
+                }
             }
-            console.log(response);
         },
         dataType: 'json'
     });
