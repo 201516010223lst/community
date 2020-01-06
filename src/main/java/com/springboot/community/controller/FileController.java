4dcef350563a15e714ef60f8dce303f9d2e1f37a
201519960121lsy <760143003@qq.com>
2020-01-06 17:00:46 +0800

    上传图片-使用SDK上传图片

--- src/main/java/com/springboot/community/controller/FileController.java
+++ src/main/java/com/springboot/community/controller/FileController.java
@@ -1,15 +1,32 @@
 package com.springboot.community.controller;
 
 import com.springboot.community.dto.FileDTO;
+import com.springboot.community.provider.UCloudProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @Controller
 public class FileController {
+    @Autowired
+    private UCloudProvider uCloudProvider;
+
     @RequestMapping("/file/upload")
     @ResponseBody
-    public FileDTO upload(){
+    public FileDTO upload(HttpServletRequest request) {
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+        MultipartFile file = multipartRequest.getFile("editormd-image-file");
+        try {
+            uCloudProvider.upload(file.getInputStream(), file.getContentType(), file.getOriginalFilename());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         FileDTO fileDTO = new FileDTO();
         fileDTO.setSuccess(1);
         fileDTO.setUrl("/images/wechat.png");
